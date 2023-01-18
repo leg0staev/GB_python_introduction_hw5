75f753bc884468e17ad3efbda004219bad4c9a6f
legostaev <alexey.legostaev@gmail.com>
2023-01-18 09:00:22 +0300

    гавнакод исправлен

--- 04_task28.py
+++ 04_task28.py
@@ -2,18 +2,17 @@
 # Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел.
 # Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
 
-def sum(x, y: int) -> int:
+def summa(x, y: int) -> int:
     if y == 0:
         return x
-    elif y != 0:
-        return sum(x+1, y-1)
+    return summa(x+1, y-1)
 
 
 while True:
     try:
         a = int(input('enter the the first number (A): '))
         b = int(input('enter the the second number (B): '))
-        result = sum(a, b)
+        result = summa(a, b)
 
         print(f'the sum A + B -> {result}')
         break
